58116a12f25604f92a4041ee896760f0df6e9763
simps4simps <savarimani6@gmail.com>
2023-08-03 01:01:44 +0330

    Added Home component

---
+++ src/Components/Home/Home.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <>
+      <div id="charachter-preview">
+        <span id="circle"></span>
+      </div>
+
+      <div id="charachter-mods"></div>
+    </>
+  );
+};
+
+export default Home;
