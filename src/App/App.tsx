83e298cbea696d9a605628a14e8f124137dd61db
simps4simps <savarimani6@gmail.com>
2023-08-06 17:06:49 +0330

    Added Pages as exports in utils folder

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,10 +1,7 @@
 import React from "react";
 import "./App.css";
 
-import NavBar from "../Components/NavBar/NavBar";
-import Home from "../Components/Home/Home";
-import About from "../Components/About/About";
-import Contact from "../Components/Contact/Contact";
+import { About, Contact, Home, NavBar } from "../Utils/PageImports";
 
 import { Routes, Route } from "react-router-dom";
 
---
+++ src/Utils/PageImports.ts
@@ -0,0 +1,6 @@
+import NavBar from "../Components/NavBar/NavBar";
+import Home from "../Components/Home/Home";
+import About from "../Components/About/About";
+import Contact from "../Components/Contact/Contact";
+
+export { NavBar, Home, About, Contact };
