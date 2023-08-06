87cef7f91fc2f68d3337b2476c790a566e0e8b51
simps4simps <savarimani6@gmail.com>
2023-08-06 18:32:36 +0330

    Added CharachterMods with canvas

--- src/Components/CharachterMods/CharachterMods.tsx
+++ src/Components/CharachterMods/CharachterMods.tsx
@@ -1,8 +1,26 @@
-import React from "react";
+import React, { useContext } from "react";
 import "./CharachterMods.css";
 
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
+
 const CharachterMods = () => {
-  return <div>CharachterMods</div>;
+  return (
+    <div className="charachter-mods">
+      <ArrowBackIosIcon className="arrows-icon" />
+      <Canvas shadows id="canvas">
+        <OrbitControls />
+
+        <mesh>
+          <boxGeometry args={[5, 5, 5]} />
+          <meshBasicMaterial />
+        </mesh>
+      </Canvas>
+      <ArrowForwardIosIcon className="arrows-icon" />
+    </div>
+  );
 };
 
 export default CharachterMods;
