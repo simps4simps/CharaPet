09291190538e44cb0ae60ec093b54f649f757890
simps4simps <savarimani6@gmail.com>
2023-08-07 13:25:03 +0330

    Refactored code

--- src/Components/CharachterMods/CharachterMods.tsx
+++ src/Components/CharachterMods/CharachterMods.tsx
@@ -8,37 +8,16 @@ import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 import AnglesToRadians from "../../Utils/AngelsToRadians";
 
-import * as THREE from "three";
-
-type CameraHelperProps = {
-  position: number[];
-  rotation: {
-    x: number;
-    y: number;
-    z: number;
-  };
-};
-
-const CameraHelper: React.FC<CameraHelperProps> = ({ position, rotation }) => {
-  const perspectiveCamera = new THREE.PerspectiveCamera(40, 1.77, 1, 3);
-  return (
-    <group
-      position={[position[0], position[1], position[2]]}
-      rotate-y={rotation.y}
-      rotate-x={rotation.x}
-      rotate-z={rotation.z}
-    >
-      <cameraHelper args={[perspectiveCamera]} />
-    </group>
-  );
-};
-
 const CharachterMods = () => {
   return (
     <div className="charachter-mods">
       <ArrowBackIosIcon className="arrows-icon" />
       <Canvas shadows id="canvas">
-        <PerspectiveCamera makeDefault position={[0, 5, 10]} />
+        <PerspectiveCamera
+          makeDefault
+          position={[0, 10, 10]}
+          rotation={[AnglesToRadians(-40), 0, 0]}
+        />
 
         <mesh receiveShadow>
           <boxGeometry args={[5, 5, 5]} />
@@ -47,15 +26,7 @@ const CharachterMods = () => {
 
         <ambientLight />
         <directionalLight />
-        <CameraHelper
-          position={[0, 5, 10]}
-          rotation={{
-            x: AnglesToRadians(10),
-            y: AnglesToRadians(10),
-            z: AnglesToRadians(10),
-          }}
-        />
-        <OrbitControls />
+        {/* <OrbitControls /> */}
       </Canvas>
       <ArrowForwardIosIcon className="arrows-icon" />
     </div>
