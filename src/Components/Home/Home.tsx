6a5971989c04a20d2c346f430012f5eacea0e265
simps4simps <savarimani6@gmail.com>
2023-08-09 02:49:20 +0330

    Completed functianlity of Mods and preview

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -2,41 +2,61 @@ import React, { useState, useEffect } from "react";
 import "./Home.css";
 
 import CharachterMods from "../CharachterMods/CharachterMods";
-import { Model } from "../MinecraftHead/Minecraft_head";
 import { McMan } from "../McMan/McMan";
+
+import { Head } from "../IsolatedMcModels/Head";
+import { Body } from "../IsolatedMcModels/Body";
+import { Leg } from "../IsolatedMcModels/Leg";
 
 import CharachterModSlider from "../CharachterModSlider/CharachterModSlider";
 import { activeModel } from "../../Utils/Types/Types";
+import AnglesToRadians from "../../Utils/AngelsToRadians";
+
+import Textures from "../../Utils/Texures";
 
 const Home = () => {
-  const [activeHead, setActiveHead] = useState<activeModel>(null);
-  const [activeBody, setActiveBody] = useState<activeModel>(null);
-  const [activeFeet, setActiveFeet] = useState<activeModel>(null);
+  const [activeHead, setActiveHead] = useState<activeModel>();
+  const [activeBody, setActiveBody] = useState<activeModel>();
+  const [activeFeet, setActiveFeet] = useState<activeModel>();
 
   return (
     <>
       <div id="charachter-preview">
         <div id="circle"></div>
         <div className="mc-man">
-          <McMan />
+          <McMan
+            rotation={{
+              x: AnglesToRadians(20),
+              y: AnglesToRadians(-25),
+              z: 0,
+            }}
+            texture={{
+              body: activeBody?.texture || "/1.png",
+              head: activeHead?.texture || "/1.png",
+              feet: activeFeet?.texture || "/1.png",
+            }}
+          />
         </div>
       </div>
 
       <div id="charachter-mods-wrapper">
         <CharachterModSlider
-          Model={Model}
+          Model={Head}
           Wrapper={CharachterMods}
           setActive={setActiveHead}
           role="Head"
+          texture={Textures.head}
         />
         <CharachterModSlider
-          Model={Model}
+          Model={Body}
           Wrapper={CharachterMods}
           setActive={setActiveBody}
           role="Body"
+          texture={Textures.body}
         />
         <CharachterModSlider
-          Model={Model}
+          Model={Leg}
+          texture={Textures.feet}
           Wrapper={CharachterMods}
           setActive={setActiveFeet}
           role="Feet"
